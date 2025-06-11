a6c1d1b58d9cd1e624329ea5ef0a1b99e7c2d6ff
Det0x-dot <whiteshadow07@protonmail.com>
2025-06-12 01:18:04 +0900

    Update Node.js version to meet Next.js requirements (>=20.10.0)

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
     NEXT_PUBLIC_API_KEY_GOKU: process.env.NEXT_PUBLIC_API_KEY_GOKU,
     NEXT_PUBLIC_API_KEY_VEGETA: process.env.NEXT_PUBLIC_API_KEY_VEGETA,
     NEXT_PUBLIC_API_KEY_GOHAN: process.env.NEXT_PUBLIC_API_KEY_GOHAN,
-    NEXT_PUBLIC_API_KEY_TRUNKS: process.env.NEXT_PUBLIC_API_KEY_TRUNKS
-  }
-}
+    NEXT_PUBLIC_API_KEY_TRUNKS: process.env.NEXT_PUBLIC_API_KEY_TRUNKS,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
