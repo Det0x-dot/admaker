51a755dfd7b7c14e4243039b0d85f82b04f07ed9
Det0x-dot <whiteshadow07@protonmail.com>
2025-06-12 01:11:00 +0900

    Fix Next.js configuration syntax

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     unoptimized: true,
   },
-  distDir: '.next',
-  trailingSlash: true,
   env: {
     NEXT_PUBLIC_API_KEY_GOKU: process.env.NEXT_PUBLIC_API_KEY_GOKU,
     NEXT_PUBLIC_API_KEY_VEGETA: process.env.NEXT_PUBLIC_API_KEY_VEGETA,
     NEXT_PUBLIC_API_KEY_GOHAN: process.env.NEXT_PUBLIC_API_KEY_GOHAN,
-    NEXT_PUBLIC_API_KEY_TRUNKS: process.env.NEXT_PUBLIC_API_KEY_TRUNKS,
+    NEXT_PUBLIC_API_KEY_TRUNKS: process.env.NEXT_PUBLIC_API_KEY_TRUNKS
   }
+}
 
-export default nextConfig
+module.exports = nextConfig
